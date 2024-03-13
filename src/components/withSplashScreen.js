df7340c5b0cf40a3b9d7d8b2ffd098d720e0161c
cansuozdizlekli <cansuozdizlekli@gmail.com>
2024-03-14 01:20:02 +0300

    bug fix

--- src/components/withSplashScreen.js
+++ src/components/withSplashScreen.js
@@ -1,4 +1,5 @@
 import React, { Component, useState, useEffect } from 'react';
+import logo from '/Users/cansuozdizlekli/AwesomeProject/assets/icon.png';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
@@ -38,3 +39,32 @@ export default function withSplashScreen(WrappedComponent) {
   };
 }
 
+// export default function withSplashScreen(WrappedComponent) {
+
+//   return class extends Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         loading: true,
+//       };
+//     }
+
+
+//     render() {
+//       return (
+//         <View style={{ flex: 1, justifyContent: 'center', width: '100%', height: '100%' }}>
+//           <View style={{ marginTop: 190 }}>
+//             <Image source={require('/Users/cansuozdizlekli/AwesomeProject/assets/splash-icon.png')} style={{ width: '100%', height: '70%' }} />
+//           </View>
+//           <View style={{ marginBottom: 90 }}>
+//             <Text style={{
+//               color: '#9DC0BF', fontSize: 35, textAlign: 'center', fontWeight: '600', textShadowColor: 'rgba(0, 0, 0, 0.3)',
+//               textShadowOffset: { width: 1, height: 1 },
+//               textShadowRadius: 3,
+//             }}>Lotus Meditation</Text>
+//           </View>
+//         </View>
+//       );
+//     }
+//   };
+// }
